666fe84f24ce0430e61bfe9bb45810f9b71d6fde
Douglas Thiago <douglas.silva@sigma.com.br>
2020-04-06 13:30:36 -0400

    atualizacao de versão do kubectl

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ clear
 export TIMEZONE="America/Cuiaba"
 export LANGUAGE="pt_BR"
 export UNICODE="UTF-8"
-export KUBECTL_VERSION="v1.16.0"
+export KUBECTL_VERSION="v1.17.0"
 export KOMPOSE_VERSION="v1.19.0"
 export NUGET_VERSION="latest"
 export NODE_VERSION="10.17.0"
